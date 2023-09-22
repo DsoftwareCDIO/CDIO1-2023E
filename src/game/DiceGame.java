88b22e636fcb348f8396365f39456f9e2bd73143
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-09-22 14:40:46 +0200

    Game controller instantiates players and dice

--- src/game/DiceGame.java
+++ src/game/DiceGame.java
@@ -1,6 +1,13 @@
 package game;
 public class DiceGame {
+    static Player p1;
+    static Player p2;
+    //static Die d1;
+    //static Die d2;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        p1 = new Player();
+        p2 = new Player();
+        //d1 = new Die();
+        //d2 = new Die();
     }
 }
