8f643f0c5088a6510059e9a16d1e9b05dcf00ca8
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-09-22 10:58:53 +0200

    Moved java files to "game" package and added player Object

--- src/game/DiceGame.java
+++ src/game/DiceGame.java
@@ -1,4 +1,5 @@
-public class App {
+package game;
+public class DiceGame {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
---
+++ src/game/Player.java
@@ -0,0 +1,12 @@
+package game;
+public class Player {
+    private int points;
+
+    public int getPoints(){
+        return points;
+    }
+    
+    public void addPoints(int points){
+        this.points += points;
+    }
+}
