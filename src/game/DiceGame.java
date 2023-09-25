e7367125bfcedf1038cc8184feff9db439561726
Citronfly <37669985+Citronfly@users.noreply.github.com>
2023-09-25 14:09:13 +0200

    Made the doc ready for a method to see if the two dice are equal

--- src/game/DiceGame.java
+++ src/game/DiceGame.java
@@ -3,19 +3,32 @@ package game;
 public class DiceGame {
     static Player p1;
     static Player p2;
+
+    // static Die d1;
+    // static Die d2;
     public static void main(String[] args) throws Exception {
         p1 = new Player();
         p2 = new Player();
+        // d1 = new Die();
+        // d2 = new Die();
 
         Player currentPlayer = p1;
         var turn = 1;
         while (/* !winCheck() */true) {
             currentPlayer = turn % 2 == 0 ? p2 : p1;
+            // d1.roll();
+            // d2.roll();
+            // Spiller får point
+            // Skifter tur
             turn++;
         }
     }
 
-    private int getSum(Die d1, Die d2){
-        return(d1.getFaceValue + d2.getFaceValue);
+    private int getSum(Die d1, Die d2) {
+        return (d1.getFaceValue() + d2.getFaceValue());
     }
+
+    // private boolean getEqual(){
+
+    // }
 }
