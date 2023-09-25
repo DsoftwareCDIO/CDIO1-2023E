6086d45c2153bbb83a73c5a0f806e2b0d0d346a0
TheSpicyOne <144691728+TheSpicyOne@users.noreply.github.com>
2023-09-25 16:09:24 +0200

    Skabt array af 2 ints der vil blive score

---
+++ src/game/PointCollector.java
@@ -0,0 +1,19 @@
+package game;
+public class PointCollector {
+    public static void main(String[] args) {
+        var scorep1 = 0;
+        var scorep2 = 0;
+
+        var score = new int[] {scorep1, scorep2};
+        System.out.println(score[0]);           // test
+        
+
+
+    }
+    // Skal tage resultat af die og lægge den til en samlet score, for henholdsvis player 1 og 2
+
+
+
+
+}
+
