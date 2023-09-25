7b126ef10e7a96efc75d3a62096f2c8d56de3760
Citronfly <37669985+Citronfly@users.noreply.github.com>
2023-09-25 14:11:33 +0200

    added updated commends to Die.java

--- src/game/Die.java
+++ src/game/Die.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Die {
+    // init list to store values of rolls in
     List<Integer> rolls = new ArrayList();
 
     public void roll() {
@@ -13,10 +14,12 @@ public class Die {
         rolls.add(random.nextInt(1, 7));
     }
 
+    // method to get the previous rolls value
     public int preRoll() {
         return (rolls.get(rolls.size() - 2));
     }
 
+    // method to get the value of the roll just made
     public int getFaceValue() {
         return (rolls.get(rolls.size() - 1));
     }
