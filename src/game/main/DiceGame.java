842244e93688c8bf84040dd49145ece9687b02c8
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-09-26 22:54:48 +0200

    Restructured and made 1000 dice rolls test in new test package

--- src/game/main/DiceGame.java
+++ src/game/main/DiceGame.java
@@ -1,4 +1,4 @@
-package game;
+package main.game;
 import java.util.Scanner;
 
 public class DiceGame {
---
+++ src/game/test/ThousandDiceRolls.java
@@ -0,0 +1,20 @@
+package game.test;
+import game.main.Die;
+
+
+public class ThousandDiceRolls {
+    public static void main(String[] args) {
+        int sum = 0;
+        Die d1 = new Die();
+        Die d2 = new Die();
+        for (int i = 0; i < 1000; i++) {
+            d1.roll();
+            sum += d1.getFaceValue();
+            d2.roll();
+            sum += d2.getFaceValue();
+            System.out.println(d1.getFaceValue() + " " + d2.getFaceValue());
+        }
+
+        System.out.println("Median: " + sum/1000.0);
+    }
+}
