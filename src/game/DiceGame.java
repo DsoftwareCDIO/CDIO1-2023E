c1582629d6fe48e696bd9772ba543b9d34fca7ac
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-09-26 13:02:57 +0200

    Added comments to DiceGame

--- src/game/DiceGame.java
+++ src/game/DiceGame.java
@@ -6,6 +6,7 @@ public class DiceGame {
     static Die d1;
     static Die d2;
     public static void main(String[] args) throws Exception {
+        // Instantiates 2 object of class Player and of class Die
         p1 = new Player("Player 1");
         p2 = new Player("Player 2");
         d1 = new Die();
@@ -13,29 +14,30 @@ public class DiceGame {
         var scanner = new java.util.Scanner(System.in);
         scanner.useLocale(java.util.Locale.ENGLISH);
 
-        //Game I/O
+        // The game starts
         System.out.println("Velkommen til terningespillet");
         Player currentPlayer = p1;
         var turn = 1;
+        // When a player wins, the while loop stops running
         while (!winCheck(currentPlayer)) {
+            // Set currentplayer depending on turn
             currentPlayer = turn % 2 == 0 ? p2 : p1;
             System.out.print("\n" + currentPlayer.getName() + " har turen, tryk ENTER for at slå med terningerne");
             scanner.nextLine();
+            
             d1.roll();
             d2.roll();
-            //Spiller får point
+            // Current player gets points
             currentPlayer.addPoints(getSum(d1, d2));
-            //Skifter tur
             System.out.println("Der blev slået " + d1.getFaceValue() + " og " + d2.getFaceValue() + ", du har nu " + currentPlayer.getPoints() + " point");
             turn++;
         }
         
+        // Congratulates the winner, when the game ends
         System.out.println("\nTillykke " + currentPlayer.getName() + ", du har vundet spillet!");
-
-        
     }
 
-    // method to get the sum of the two dice thowed this turn
+    // Method to get the sum of the two dice thrown this turn
     private static int getSum(Die d1, Die d2) {
         return (d1.getFaceValue() + d2.getFaceValue());
     }
@@ -44,6 +46,7 @@ public class DiceGame {
 
     // }
   
+    // Checks wthether a player has won the game
     private static boolean winCheck(Player currentPlayer){
         return currentPlayer.getPoints() >= 40;
     }
