5735f6f2725a56bb43946424f7e813516ff8f747
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-09-27 17:05:50 +0200

    Added test for two 6's two times in a row

---
+++ src/game/test/TestDiceGame.java
@@ -0,0 +1,76 @@
+package game.test;
+import game.main.Player;
+import java.util.Scanner;
+
+public class TestDiceGame {
+    static Player p1;
+    static Player p2;
+    static TestDie d1;
+    static TestDie d2;
+
+    public static void main(String[] args) throws Exception {
+        // Instantiates 2 object of class Player and of class Die
+        p1 = new Player("Player 1");
+        p2 = new Player("Player 2");
+        d1 = new TestDie();
+        d2 = new TestDie();
+        Scanner scanner = new Scanner(System.in);
+
+        // The game starts
+        System.out.println("Welcome to the Dice Game");
+        Player currentPlayer = p1;
+        int turn = 1;
+        // When a player wins, the while loop stops running
+        do {
+            // Set currentplayer depending on turn
+            currentPlayer = turn % 2 == 0 ? p2 : p1;
+            System.out.print("\n" + currentPlayer.getName() + "'s turn, press ENTER to roll the dice");
+            scanner.nextLine();
+
+            d1.roll();
+            d2.roll();
+            // Checks if roll is double ones and current player gets points
+            if (getSum(d1, d2) == 2){
+                currentPlayer.resetPoints();
+                System.out.println("You rolled double ones. You now have " + currentPlayer.getPoints() + " points");
+            } else {
+                currentPlayer.addPoints(getSum(d1, d2));
+                System.out.println("You rolled " + d1.getFaceValue() + " and " + d2.getFaceValue() + ", you now have "
+                    + currentPlayer.getPoints() + " points");
+            }
+
+            if (getEqual(d1, d2)) {
+                turn--;
+                System.out.println("You rolled two of a kind, so you get an extra turn");
+            }
+            turn++;
+        } while (!winCheck(currentPlayer, d1, d2));
+
+        // Congratulates the winner, when the game ends
+        System.out.println("\nCongratulations " + currentPlayer.getName() + ", you won the game!");
+        scanner.nextLine();
+        scanner.close();
+    }
+
+    // Method to get the sum of the two dice thrown this turn
+    private static int getSum(TestDie d1, TestDie d2) {
+        return (d1.getFaceValue() + d2.getFaceValue());
+    }
+    
+    // Checks whether a player has won the game
+    public static boolean winCheck(Player currentPlayer, TestDie d1, TestDie d2) {
+        if (currentPlayer.getPoints() >= 40) {
+            return true;
+        }
+        if (getSum(d1, d2) == 12 && d1.preRoll() == 6 && d2.preRoll() == 6) {
+            System.out.println("You rolled two 6's two times in a row");
+            return true;
+        }
+        return false;
+    }
+
+    // method to check if the to dice in this throw is equal
+    private static boolean getEqual(TestDie d1, TestDie d2) {
+        return d1.getFaceValue() == d2.getFaceValue();
+    }
+}
