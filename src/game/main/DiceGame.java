96d88b062706746b23f07cb85982ff286e2044ee
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2023-09-29 15:16:19 +0200

    Changed rules format

--- src/game/main/DiceGame.java
+++ src/game/main/DiceGame.java
@@ -16,10 +16,13 @@ public class DiceGame {
         Scanner scanner = new Scanner(System.in);
 
         // The game starts
+        System.out.println("---------------------------------------------------------------------");
         System.out.println("Welcome to the Dice Game");
+        
         System.out.println("First to get two of a kind with 40 or more points win");
         System.out.println("Two of a kind gives an extra turn, but two 1's resets your points");
         System.out.println("A pair of 6's two times in a row also wins the game");
+        System.out.println("---------------------------------------------------------------------");
         Player currentPlayer = p1;
         int turn = 1;
         // When a player wins, the while loop stops running
